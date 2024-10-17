a76fc59e804c584f8aed8c85122f6b9ee4b6a8bf
Zenithao <adrien.le.pober@gmail.com>
2024-10-17 12:33:12 +0200

    add template banner

---
+++ web/app/themes/custom/template-banner.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Template Name: Page avec bannière
+ * Template Post Type: page, post
+ */
+?>
+
+<?php get_header() ?>
+
+<?php if (have_posts()): ?>
+    <?php while (have_posts()): the_post(); ?>
+        <p>Ici la bannière</p>
+        <h1><?= the_title(); ?></h1>
+        <p>
+            <img src="<?= the_post_thumbnail_url() ?>" alt="" style="width:100%; height:auto;">
+        </p>
+        <?= the_content(); ?>
+<?php endwhile;
+endif; ?>
+
+<?php get_footer() ?>
