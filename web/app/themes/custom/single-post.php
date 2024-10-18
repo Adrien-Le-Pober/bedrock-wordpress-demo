f5548e605e51b1c81ece1552d8776e7ea3d4af67
Zenithao <adrien.le.pober@gmail.com>
2024-10-18 21:42:48 +0200

    add relative posts on single post page with WQ_Query

--- web/app/themes/custom/single-post.php
+++ web/app/themes/custom/single-post.php
@@ -1,18 +1,56 @@
-<?php get_header() ?>
+<?php
+
+use App\Metaboxes\SponsoMetaBox;
+
+get_header() ?>
 
 <?php if (have_posts()): ?>
     <?php while (have_posts()): the_post(); ?>
         <h1><?= the_title(); ?></h1>
 
-        <?php if(get_post_meta(get_the_ID(), App\Metaboxes\SponsoMetaBox::META_KEY, true) === "1"): ?>
+        <?php if (get_post_meta(get_the_ID(), App\Metaboxes\SponsoMetaBox::META_KEY, true) === "1"): ?>
             <div class="alert alert-info">
                 Cet article est sponsorisé
             </div>
         <?php endif ?>
         <p>
-            <img src="<?= the_post_thumbnail_url()?>" alt="" style="width:100%; height:auto;">
+            <img src="<?= the_post_thumbnail_url() ?>" alt="" style="width:100%; height:auto;">
         </p>
         <?= the_content(); ?>
+
+        <h2>Articles relatifs</h2>
+
+        <div class="row">
+            <?php
+            $sports = array_map(function($term) {
+                return $term->term_id;
+            }, get_the_terms(get_post(), 'sport'));
+            $query = new WP_Query([
+                'post__not_in' => [get_the_ID()], // article à exclure
+                'post_type' => 'post', // récupérer seulement les articles
+                'posts_per_page' => 3,
+                'orderby' => 'rand', // random
+                'tax_query' => [
+                    [
+                        'taxonomy' => 'sport',
+                        'terms' => $sports
+                    ]
+                ],
+                'meta_query' => [
+                    [
+                        'key' => SponsoMetaBox::META_KEY,
+                        'compare' => 'NOT EXISTS'
+                    ]
+                ]
+            ]);
+            while ($query->have_posts()) : $query->the_post();
+            ?>
+                <?php get_template_part('parts/card', 'post'); ?>
+            <?php endwhile;
+            wp_reset_postdata();
+            ?>
+        </div>
+
 <?php endwhile;
 endif; ?>
 
